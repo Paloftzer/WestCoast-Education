b8ddce3f859a3a2a36968d08c0550979231ba9f4
Paloftzer <phille04@outlook.com>
2024-10-08 19:43:04 +0200

    Added static to the methods

--- models/Student.cs
+++ models/Student.cs
@@ -3,7 +3,7 @@
 public class Student : Person
 {
     // for comments see Course.cs it is the original file and most of the other files will be nearly identical
-    public void AddStudent(string firstName, string lastName, string phoneNumber, string personalIdentityNumber, string address, string postalCode, string city)
+    public static void AddStudent(string firstName, string lastName, string phoneNumber, string personalIdentityNumber, string address, string postalCode, string city)
     {
         List<Student> Student = FileManager.ReadFromFile<Student>();
         Student.Add(new Student { FirstName = firstName, LastName = lastName, PhoneNumber = phoneNumber, PersonalIdentityNumber = personalIdentityNumber, Address = address, PostalCode = postalCode, City = city });
@@ -11,7 +11,7 @@ public class Student : Person
         FileManager.WriteToFile(Student);
     }
 
-    public void ListStudents()
+    public static void ListStudents()
     {
         List<Student> students = FileManager.ReadFromFile<Student>();
         Console.WriteLine("List of all attending Students:");
